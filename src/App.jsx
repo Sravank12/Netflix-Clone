aecf5aa778f4907a6fda457158a8cf6a0bb60b2a
K.SRAVAN KUMAR <113578253+Sravank12@users.noreply.github.com>
2023-01-02 22:11:24 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./app.scss"
+import Home from "./pages/home/Home";
+import Register from "./pages/register/Register";
+import Watch from "./pages/watch/Watch";
+import Login from "./pages/login/Login";
+
+const App = () => {
+  return <Home/>;
+};
+
+export default App;
---
+++ src/pages/watch/Watch.jsx
@@ -0,0 +1,20 @@
+import { ArrowBackOutlined } from "@material-ui/icons";
+import "./watch.scss";
+
+export default function Watch() {
+  return (
+    <div className="watch">
+      <div className="back">
+        <ArrowBackOutlined />
+        Home
+      </div>
+      <video
+        className="video"
+        autoPlay
+        progress
+        controls
+        src="https://www.youtube.com/watch?v=OKBMCL-frPU"
+      />
+    </div>
+  );
+}
